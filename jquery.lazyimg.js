a437c1b935ab33bb7d959e926c6174901f88d42a
Piotr Cichosz <piotr@fronthero.pl>
2014-02-05 14:10:10 +0100

    Added some comments.

--- jquery.lazyimg.js
+++ jquery.lazyimg.js
@@ -1,29 +1,37 @@
-$.fn.lazyimg = function(options) {
+(function($){
 
-  if(!options) {
-    options = {};
-  }
-  options.offset = options.offset || 0;
-  options.attr = options.attr || 'lazy-img-url';
+  $.fn.lazyimg = function(options) {
+  
+    // * Check options and set defaults.
+    if(!options) {
 
-  $(this).each(function() {
-    
-    var $img = $(this),
-      windowHeight,
-      scrollTop,
-      imgTop;
-    
-    if(!$img.is(':visible')) {
-      return;
+      options = {};
     }
-    
-    windowHeight = $(window).height(),
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop,
-    imgTop = $img.offset().top;
 
-    if (scrollTop + windowHeight + options.offset > imgTop) {
-            
-    	$img.attr('src', $img.attr(options.attr));
-    }
-  });
-};
+    options.offset = options.offset || 0;
+    options.attr = options.attr || 'lazy-img-url';
+    // ---
+  
+    $(this).each(function() {
+      
+      var $img = $(this), // * The image.
+        windowHeight, // * Window height.
+        scrollTop, // * Scroll top (position).
+        imgTop; // * Image top position.
+  
+      if(!$img.is(':visible')) {
+
+        return;
+      }
+  
+      windowHeight = $(window).height(),
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop,
+      imgTop = $img.offset().top;
+  
+      if (scrollTop + windowHeight + options.offset > imgTop) {
+  
+      	$img.attr('src', $img.attr(options.attr));
+      }
+    });
+  };
+)(jQuery);
