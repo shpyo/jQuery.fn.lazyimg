8a79224d6354c46511cfc4d0ad0d1359874d27d9
Piotr Cichosz <piotr@fronthero.pl>
2014-02-05 09:56:37 +0100

    Added first variables

--- jquery.lazyimg.js
+++ jquery.lazyimg.js
@@ -1,3 +1,16 @@
-$.fn.lazyimg = function(options) {
+$.fn.lazyimg = function(offset) {
 
+  offset = offset || 0;
+
+  $(this).each(function() {
+    
+    var $img = $(this),
+      windowHeight,
+      scrollTop,
+      imgTop;
+    
+    if(!$img.is(':visible')) {
+      return;
+    }
+  });
 };
